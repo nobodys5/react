14c082ee979546597444f8920cb94db23142ab16
nobodys5 <samudi2@naver.com>
2024-08-29 11:41:04 +0900

    수정

---
+++ src/router/PathVariable.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { useParams } from 'react-router'
+
+export default function PathVariable() {
+
+    // useParams :
+    // - 경로 변수에 해당하는 값을 읽을때 사용하는 react-router의 훅 함수
+
+    const { name } = useParams();
+    console.log(name);
+    return (
+        <div>
+        NAME : {name}
+    </div>
+    
+)
+}
